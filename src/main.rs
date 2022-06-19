e1396ced2f8af14e8b294a6de694e6e6753669fb
BartoszMiz <bartmiz.2004@gmail.com>
2022-06-19 14:47:17 +0200

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::{net::TcpListener, io::Write};
+use log::{debug, info, warn, error};
 
 const BIND_ADDRESS: &str = "127.0.0.1";
 const BIND_PORT: u32 = 8080;
 
 fn main() {
+	env_logger::init();
 	let address = format!("{}:{}", BIND_ADDRESS, BIND_PORT);
 	let tcp_listener = match TcpListener::bind(&address) {
 		Ok(listener) => {
-			println!("Server listening on {}", &address);
+			info!("Server listening on {}", &address);
 			listener
 		}
 		Err(e) => {
-			println!("Failed to bind {} - {}", &address, e);
+			error!("Failed to bind {} - {}", &address, e);
 			return;
 		}
 	};
@@ -19,13 +21,13 @@ fn main() {
 	for stream in tcp_listener.incoming() {
 		match stream {
 			Ok(mut stream) => {
-				println!("Incoming connection from: {}", stream.peer_addr().unwrap());
+				info!("Incoming connection from: {}", stream.peer_addr().unwrap());
 				stream.write_all(b"HTTP/1.1 418 I'm a teapot\r\n").unwrap();
 				stream.flush().unwrap();
 				stream.shutdown(std::net::Shutdown::Both).unwrap();
 			},
 			Err(e) => {
-				println!("Connection failed: {}", e);
+				warn!("Connection failed: {}", e);
 			}
 		}
 	}
